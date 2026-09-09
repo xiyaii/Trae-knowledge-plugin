98bec4caa7e3283c88b4b7e31ee4c6875579029c
yifei.eng <yifei.eng@outlook.com>
2026-09-09 11:20:56 +0800

    升级旗舰版知识库

--- admin-service/kb_proxy.go
+++ admin-service/kb_proxy.go
@@ -15,7 +15,7 @@ import (
 
 var kbDomain = "api-knowledgebase.mlp.cn-beijing.volces.com"
 var kbServiceChatPath = "/api/knowledge/service/chat"
-var kbServiceResourceID = "kb-service-43adcb20b1bb63a1" //问答服务
+var kbServiceResourceID = "kb-service-1ca4fa064b1a45d1" //问答服务
 
 // kbHTTPClient 全局复用连接池，避免每次请求重新建立 TCP 连接
 // - MaxIdleConnsPerHost: 10 对单 host 足够，超过则新建连接
